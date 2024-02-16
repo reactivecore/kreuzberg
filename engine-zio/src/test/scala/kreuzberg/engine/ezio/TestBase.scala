0ad3e8fa00a483ba872254533e3c88b3bb082037
Norbert Schultz <norbert.schultz@reactivecore.de>
2024-02-16 15:20:26 +0000

    KRZ-174 Remove ZIO Engine

--- engine-zio/src/test/scala/kreuzberg/engine/ezio/TestBase.scala
+++
@@ -1,6 +0,0 @@
-package kreuzberg.engine.ezio
-
-import org.scalatest.matchers.should.Matchers
-import org.scalatest.flatspec.AnyFlatSpec
-
-abstract class TestBase extends AnyFlatSpec with Matchers
